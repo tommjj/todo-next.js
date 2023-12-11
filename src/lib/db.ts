4aef517805b23c8caded0855e46647dadbc34a72
nndang <nndang.sc@gmail.com>
2023-12-11 22:06:05 +0700

    add prisma global

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-const logs = { log: ['query', 'info'] }
+const logs = { log: ['query', 'info'] };
 
-const db = new PrismaClient();
+let db: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    db = new PrismaClient();
+} else {
+    let globalWithPrisma = global as typeof globalThis & {
+        prisma: PrismaClient;
+    };
+    if (!globalWithPrisma.prisma) {
+        globalWithPrisma.prisma = new PrismaClient();
+    }
+    db = globalWithPrisma.prisma;
+}
 
 export default db;
