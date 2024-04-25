7114be47eb98279f90b60600e804a093d47adf15
nndang <nndang.sc@gmail.com>
2024-04-25 17:53:52 +0700

    fix overflow

--- src/app/tasks/template.tsx
+++ src/app/tasks/template.tsx
@@ -2,7 +2,7 @@ import SideNav from '@/components/ui/nav/nav';
 
 function Template({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex relative w-screen h-screen">
+        <div className="flex relative w-screen h-screen overflow-x-hidden">
             <SideNav />
             <div className="flex grow justify-center h-full overflow-hidden custom-scrollbar bg-main-bg-color dark:bg-main-bg-color-dark ">
                 {children}
