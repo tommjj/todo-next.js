e9e68f38f289e60ab520feb5ab614b11899e38fd
nndang <nndang.sc@gmail.com>
2024-05-12 21:52:16 +0700

    fix detail resize

--- src/components/ui/details/detail.tsx
+++ src/components/ui/details/detail.tsx
@@ -81,7 +81,7 @@ export default function DetailsContainer({ id }: { id?: string }) {
                 ></div>
                 <ResizeContainer
                     className={cn(
-                        'max-w-[360px] bg-nav-bg-color dark:bg-nav-bg-color-dark h-full z-50',
+                        'max-w-[360px] md:max-w-[900px] bg-nav-bg-color dark:bg-nav-bg-color-dark h-full z-50',
                         {
                             'max-w-[360px] md:max-w-[360px] -ml-[360px] translate-x-[360px]':
                                 !task,
